be40dac3839caa7c8c8392af4112b4eafc276e59
Mike <winiar93@gmail.com>
2021-04-24 00:58:19 +0200

    in script take offer name, price and link

--- datacollect/datacollect/spiders/quotes_spider.py
+++ datacollect/datacollect/spiders/quotes_spider.py
@@ -6,11 +6,12 @@ class Bring(scrapy.Spider):
 
 
     def parse(self, response):
-        for data in response.css('wrap'):
+        for data in response.css('.wrap'):
             yield {
-                "name": data.css('strong::text').get(),
+                "name":data.css('strong::text').get(),
                 "price": data.css('p.price').get(),
-                "link" : data.css('a.marginright5.link.linkWithHash.detailsLink').attrib['href']
+                #"link": data.css('table.offers.redesigned.userobserved-list.space.h3.a.listHandler.table.offers.redesigned.space.h3.a').attrib['href']
+                "link":data.xpath('//*[@id="offers_table"]/tbody/tr[12]/td/div/table/tbody/tr[1]/td[2]/div/h3/a/@href').extract()
             }
         # f = open('dane.txt','a')
         # f.write(str(title)+"\n")
