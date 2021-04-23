52d3d6d60e37b1ce1bc6f537e25be173cfb8fdb8
Mike <winiar93@gmail.com>
2021-04-24 01:08:14 +0200

    correct output data in script

--- datacollect/datacollect/spiders/quotes_spider.py
+++ datacollect/datacollect/spiders/quotes_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import re
 class Bring(scrapy.Spider):
     name = "quotes"  # nazwa programu do uruchomienia w terminalu
     start_urls = ['https://www.olx.pl/oferty/q-commodore-64/']
@@ -9,7 +9,7 @@ class Bring(scrapy.Spider):
         for data in response.css('.wrap'):
             yield {
                 "name":data.css('strong::text').get(),
-                "price": data.css('p.price').get(),
+                "price": int(re.search(r'\d+', data.css('p.price').get()).group()),
                 #"link": data.css('table.offers.redesigned.userobserved-list.space.h3.a.listHandler.table.offers.redesigned.space.h3.a').attrib['href']
                 "link":data.xpath('//*[@id="offers_table"]/tbody/tr[12]/td/div/table/tbody/tr[1]/td[2]/div/h3/a/@href').extract()
             }
