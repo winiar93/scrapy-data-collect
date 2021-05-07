44b236a170dd65db9e213613e31ae6883b240e55
Michał Winiarski <m.winiarski@hta.pl>
2021-05-07 18:49:35 +0200

    add page iter

--- commodore/commodore/spiders/quotes_spider.py
+++ commodore/commodore/spiders/quotes_spider.py
@@ -2,20 +2,21 @@ import scrapy
 import re
 class Bring(scrapy.Spider):
     name = "quotes"  # nazwa programu do uruchomienia w terminalu
-    start_urls = ['https://www.olx.pl/oferty/q-commodore-64/']
+    start_urls = ['https://www.olx.pl/oferty/q-commodore-64/?search%5Bfilter_float_price%3Afrom%5D=200&page=1',
+                  'https://www.olx.pl/oferty/q-commodore-64/?search%5Bfilter_float_price%3Afrom%5D=200&page=2',
+                  'https://www.olx.pl/oferty/q-commodore-64/?search%5Bfilter_float_price%3Afrom%5D=200&page=2'
+                  ]
 
 
     def parse(self, response):
-        for data in response.css('.wrap'):
+        for data in response.css('div.offer-wrapper'):
             yield {
                 "name":data.css('strong::text').get(),
-                "price": int(re.search(r'\d+', data.css('p.price').get()).group()),
-                #"link": data.css('table.offers.redesigned.userobserved-list.space.h3.a.listHandler.table.offers.redesigned.space.h3.a').attrib['href']
-                "link":data.xpath('//*[@id="offers_table"]/tbody/tr[12]/td/div/table/tbody/tr[1]/td[2]/div/h3/a/@href').extract()
+                "price":(data.css('strong::text').extract()[1]).replace('zł',''),
+                "link":data.css('a::attr(href)').extract()[0]
+
+
             }
-        # f = open('dane.txt','a')
-        # f.write(str(title)+"\n")
-        # f.close()
-
-
-        
+        next_page = response.css('a.pageNextPrev::attr(href)').extract()[0]
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
